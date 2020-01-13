e46a7cac765a5c12e54c62018ee511b6c7d9d6ce
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-13 13:20:39 +0200

    navbar added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,17 +1,20 @@
 import React from 'react';
+
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const toolbar = (props) => {
-    return (
-        <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <Logo/>
-            <nav>
-                ...
-            </nav>
-        </header>
-    );
-};
+const toolbar = ( props ) => (
+    <header className={classes.Toolbar}>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+        </nav>
+    </header>
+);
 
 export default toolbar;
